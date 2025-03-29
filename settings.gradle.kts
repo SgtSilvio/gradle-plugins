f8937aeadef890481a726203f045cf1a5bc16db9
Silvio Giebl <gieblsilvio@outlook.com>
2025-03-30 00:43:17 +0100

    Add gradle-maven-central-publisher

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "gradle-plugins"
 includeBuild("../android-retrofix")
 includeBuild("../gradle-defaults")
 includeBuild("../gradle-javadoc-links")
+includeBuild("../gradle-maven-central-publishing")
 includeBuild("../gradle-metadata")
 includeBuild("../gradle-oci")
 includeBuild("../gradle-proguard")
